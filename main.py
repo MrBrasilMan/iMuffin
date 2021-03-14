787a845ddfe511a83148cbe6edb9073848b917f8
MrBrazil <lucasfrias878@gmail.com>
2021-03-14 16:34:25 +0000

    Added better readability and error detection for webpages

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 #By Lucas Frias 2021
 #Please read.me for license and other info
 #iMuffin aims to be a quick, lightweight browser portable to most Linux/Unix systems
+###############
+### Startup ###
+###############
+
 try:
   import sys
   import requests
@@ -10,22 +14,47 @@ except:
   print ("error (1): install sys, requests, and os modules using pip\n if this problem persists, make sure parser.py is included with your installation.")
 #Starting variables
 url = "home"
-version = 0.1
+version = 0.2
+
+
+
+#############
+## Main #####
+############
 while True:
   #Bar
   print ("iMuffin - " + url)
   print ("-----------------------------")
   #Main display
+  #This is home, which will display some home starting text
+
+
+#################
+###### Pages ####
+#################
+
   if url == "home":
     print ("iMuffin Home Page")
     print ("Thanks for trying out iMuffin Aplha!")
     print ("You are running version " + str(version))
+  #A failure page if things happen to go wrong loading the URL
   elif url == "error":
     print ("Uh Oh\nMalformed request")
+    #Try and display a status code, otherwise display a "no response message"
+    try:
+      print ("Response: " + wanted.status_code)
+    except:
+      print ("No Response Sent. Is it a valid URL?")
+
+
+#########################
+### Request Handling ###
+########################
+
   else:
     print (parser.rm_p(wanted.text))
   url = input(">")
-  #Pass if the url is equal to home
+  #Pass if the url is equal to home(the page render will render this)
   if url == "home":
     pass
   else:
@@ -33,6 +62,5 @@ while True:
     try:
       wanted = requests.get(url)
     except:
-      #Otherwise display the error page. FOr now, we don't really care what the error is.
-      print ("error (2): cant get webpage")
+      #Otherwise display the error page. For now, we don't really care what the error is. We set this as the url so the web render does not have to deal with an empty object
       url = "error"
